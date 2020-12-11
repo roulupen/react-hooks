614ba61b628b0d18a3d9893826e8aa98cb052991
Upendra Roul <roul@adobe.com>
2020-12-11 19:49:40 +0530

    Error Handling and Spinner.

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState();
 
   // Since we are already fethcing the initial data in search component so we don't need to
   // initialize the data, so the code is commented but kept for reference to how to call
@@ -26,9 +29,14 @@ function Ingredients() {
   //     })
   // }, []);
 
-  useEffect(() => console.log('Ingredients Component rendered.'));
+  
+  useEffect(
+    () => console.log('Ingredients Component rendered.'),
+    [userIngredients]
+  );
 
   const addIngredienthandler = (ingredient) => {
+    setIsLoading(true);
     fetch('https://react-hook-90f4c-default-rtdb.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
@@ -36,22 +44,31 @@ function Ingredients() {
         'Content-Type': 'application/json'
       }
     }).then(response => {
+      setIsLoading(false);
       return response.json();
     }).then(responseData => {
       setUserIngredients(prevIngredient => [
         ...prevIngredient,
         {id: responseData.name, ...ingredient}
       ]);
+    }).catch(error => {
+      setErr('Something went wrong!');
+      setIsLoading(false);
     });
   };
 
   const removeIngredienthandler = (ingredientId) => {
+    setIsLoading(true);
     fetch(`https://react-hook-90f4c-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`, {
       method: 'DELETE'
     }).then(response => {
+      setIsLoading(false);
       setUserIngredients(prevIngredients =>
         prevIngredients.filter(item => item.id !== ingredientId)
       );
+    }).catch(error => {
+      setErr('Something went wrong!');
+      setIsLoading(false);
     });
   };
 
@@ -59,10 +76,14 @@ function Ingredients() {
     setUserIngredients(ingredients);
   }, []);
 
+  const clearError = () => {
+    setErr(null);
+  };
+
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredienthandler} />
-
+      {err && <ErrorModal onClose={clearError}>{err}</ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredienthandler} loading={isLoading} />
       <section>
         <Search onLoadIngredients={ingredientFilterHandler} />
         <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredienthandler} />
